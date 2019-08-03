b13c0784f60178a98a6f4b88cd7e8798ea80feaa
Kona Marie <ambermoreyra@gmail.com>
2019-08-03 12:18:56 -0600

    updated example.js with sequelize schema

--- models/example.js
+++ models/example.js
@@ -1,7 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Survey = sequelize.define("Survey", {
+	name: DataTypes.STRING,
+	county: DataTypes.STRING,
+    income: DataTypes.INTEGER,
+    menstruation_m: DataTypes.INTEGER,
+    menstruation_y: DataTypes.INTEGER,
+    pregnancy_m: DataTypes.INTEGER,
+    pregnancy_y: DataTypes.INTEGER,
+    cosmetics_m: DataTypes.INTEGER, 
+    cosmetics_y: DataTypes.INTEGER,
+    garments_m: DataTypes.INTEGER,
+    garments_y: DataTypes.INTEGER,
+    feedback: DataTypes.TEXT,
+
   });
-  return Example;
+  return Survey;
 };
+
+//schema
