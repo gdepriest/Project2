8a0aee22a5799da4da04499b283150d92a43eaeb
Kona Marie <ambermoreyra@gmail.com>
2019-08-06 14:49:06 -0600

    connected nav bar and got form inputs to post to table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,52 @@
-var surveyController = require("../controllers/survey-controller")
-module.exports = function(app) {
+var db = require("../models");
+
+// var surveyController = require("../controllers/survey-controller")
+module.exports = function (app) {
   // Get all Survey Data
-  app.get("/api/survey", function(req, res) {
-    surveyController.findAll(req, res)
+  app.get("/api/survey", function (req, res) {
+    db.Survey.findAll({
+
+    }).then(function (surveys) {
+      res.json(surveys);
+    })
   });
 
   // Create a new Survey
-  app.post("/api/survey", function(req, res) {
-    surveyController.createOne(req, res)
+  app.post("/api/survey", function (req, res) {
+    var {
+      name,
+      county,
+      income,
+      menstruation,
+      menstruation_monthly,
+      pregnancy,
+      pregnancy_monthly,
+      cosmetics,
+      cosmetics_monthly,
+      garments,
+      garments_monthly,
+      feedback
+    } = req.body;
+
+    var newSurvey= {
+      name,
+      county,
+      income,
+      menstruation,
+      menstruation_monthly,
+      pregnancy,
+      pregnancy_monthly,
+      cosmetics,
+      cosmetics_monthly,
+      garments,
+      garments_monthly,
+      feedback
+    };
+
   });
 
   // Delete a Survey by id
-  app.delete("/api/survey/:id", function(req, res) {
-    surveyController.deleteOne(req, res)
-  });
+  // app.delete("/api/survey/:id", function(req, res) {
+  //   surveyController.deleteOne(req, res)
+  // });
 };
