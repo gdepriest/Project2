28bcbbdb9916d22014fd04bac1da08f3224aa127
Kona Marie <ambermoreyra@gmail.com>
2019-08-06 20:13:47 -0600

    really got the form input to post to db table

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE surveydb;
-INSERT INTO survey (name, county, income, menstruation_m, menstruation_y, pregnancy_m, pregnancy_y, cosmetics_m, cosmetics_y, garments_m, garments_y, feedback) VALUES 
-('Genevieve', 'Adams', 51000, NULL, 60, NULL, 0, NULL, 200, NULL, 120, 'none');
-INSERT INTO survey (name, county, income, menstruation_m, menstruation_y, pregnancy_m, pregnancy_y, cosmetics_m, cosmetics_y, garments_m, garments_y, feedback) VALUES ('Amber', 'Adams', 15000, NULL, 20, NULL, 0, NULL, 20, NULL, 200, 'none');
-INSERT INTO survey (name, county, income, menstruation_m, menstruation_y, pregnancy_m, pregnancy_y, cosmetics_m, cosmetics_y, garments_m, garments_y, feedback) VALUES ('Christelle', 'Denver', 75000, NULL, 240, NULL, 0, NULL, 150, NULL, 500, 'none');
